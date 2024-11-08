a6ef1c9358c3b66d72396896873a11b6f0f0de52
enniemilia <130854866+enniemilia@users.noreply.github.com>
2024-11-08 12:20:25 +0200

    comments

--- awap-frontend/config/config.js
+++ awap-frontend/config/config.js
@@ -1,2 +1,6 @@
 export const BACKEND_URL = 'http://localhost:3000';
 export const FRONTEND_URL = 'http://localhost:5500';
+
+/* if you need the back end url in front you can do it by
+
+import { BACKEND_URL } from "path/to/config" */
